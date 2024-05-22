34e507498cfc67e26f6787aa95f4e4c790484dff
Enrico Papsch Di Giacomo <ricopapsch@hotmail.com>
2024-05-21 21:37:06 -0300

    feat: add env variable to db

--- app/database/db.py
+++ app/database/db.py
@@ -1,11 +1,24 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://superuser:superpassword@localhost/postgres"
+user = os.environ["DB_USER"]
+password = os.environ["DB_PASSWORD"]
+endpoint = os.environ["DB_ENDPOINT"]
+database = os.environ["DB_NAME"]
+port = os.environ["DB_PORT"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+url_obj = URL.create(
+    drivername="postgresql+psycopg2",
+    username=user,
+    password=password,
+    host=endpoint,
+    port=int(port),
+    database=database
+)
+
+engine = create_engine(url=url_obj)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()

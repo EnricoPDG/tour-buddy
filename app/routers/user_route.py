ad7a475199db44cc7ed9eb6279d97aa91d882e44
CaioCoelhoChaves <caiocoelhochaves@gmail.com>
2024-05-26 18:18:09 -0300

    user request divided by email and id

--- app/routers/user_route.py
+++ app/routers/user_route.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
-from schema import UserSchemaRequest, UserSchemaResponse
+from app.schema import UserSchemaRequest, UserSchemaResponse
 from repository import UserRepository
 from database import get_db
 from loguru import logger
@@ -37,7 +37,33 @@ async def create_user(user: UserSchemaRequest, db: Session = Depends(get_db)):
     )
 
 
-@router.get("/profile/{email}", response_model=UserSchemaResponse, status_code=200)
+@router.get("/profile/{user_id}", response_model=UserSchemaResponse, status_code=200)
+async def fetch_user_profile_by_id(user_id: str, db: Session = Depends(get_db)):
+    try:
+        user = UserRepository.get_user(db=db, user_id=user_id)
+        if user is None:
+            raise HTTPException(status_code=404, detail=f"Erro: o usuário não foi encontrado")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"error: {e}")
+
+    logger.info(f"User response: {create_user}")
+    return UserSchemaResponse(
+        id=user.id,
+        email=user.email,
+        name=user.name,
+        username=user.username,
+        type=user.type,
+        cellphone_number=user.cellphone_number,
+        birthday=user.birthday,
+        cpf=user.cpf,
+        avatar_url=user.avatar_url,
+        state=user.state,
+        city=user.city,
+
+    )
+
+
+@router.get("/{email}", response_model=UserSchemaResponse, status_code=200)
 async def fetch_user_profile_by_id(email: str, db: Session = Depends(get_db)):
     try:
         user = UserRepository.get_user(db=db, email=email)
@@ -60,3 +86,4 @@ async def fetch_user_profile_by_id(email: str, db: Session = Depends(get_db)):
         state=user.state,
         city=user.city,
     )
+

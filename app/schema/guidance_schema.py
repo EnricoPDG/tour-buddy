b023b2fd086036a3ae8616dcad5293685816711c
Thomas Nicola <thomasnicola@outlook.com>
2024-06-06 23:50:33 -0300

    -fix: fixed post guidance integration.

--- app/schema/guidance_schema.py
+++ app/schema/guidance_schema.py
@@ -4,29 +4,32 @@ from uuid import UUID
 from typing import Optional
 import schema.guidance_destination_schema as g
 
+
 class HolderSchema(BaseModel):
     id: UUID
     name: str
     username: str
-    image: str
+    image: Optional[str] = None
+
 
 class GuidanceSchemaBase(BaseModel):
     title: str
     description: str
-    rating: float
     state: str
-    city: Optional[str] = None 
+    city: Optional[str] = None
     approximately_value: float
-    destinations: List[g.GuidanceDestinationSchemaResponse]
+    destinations: List[g.GuidanceDestinationSchemaBase]
 
 
 class GuidanceSchemaRequest(GuidanceSchemaBase):
-    ...
+    owner_id: UUID
 
 
 class GuidanceSchemaResponse(GuidanceSchemaBase):
     id: UUID
     holder: HolderSchema
+    rating: float
+    destinations: List[g.GuidanceDestinationSchemaResponse]
 
     class Config:
         orm_mode = True

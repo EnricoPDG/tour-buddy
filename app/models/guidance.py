84930da4e523ea37efd4b9156b0e83d7850cc181
CaioCoelhoChaves <caiocoelhochaves@gmail.com>
2024-06-08 03:54:11 -0300

    description in Guidance model now is nullable

--- app/models/guidance.py
+++ app/models/guidance.py
@@ -7,7 +7,7 @@ class Guidance(Base):
     __tablename__ = "guidance"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     title = Column(String, nullable=False)
-    description = Column(String, nullable=False)
+    description = Column(String, nullable=True)
     state = Column(String, nullable=False)
     city = Column(String, nullable=False)
     approximately_value = Column(Float, nullable=True)
